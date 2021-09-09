77c20ac5be60d88486adf25cf9781554f071acd4
ZaidZakir <zaidzakir9@gmail.com>
2021-09-09 15:08:44 +0800

    coroutine rule comment

--- app/src/test/java/com/zaidzakir/androidtestingsample/MainCoroutineRule.kt
+++ app/src/test/java/com/zaidzakir/androidtestingsample/MainCoroutineRule.kt
@@ -12,6 +12,12 @@ import org.junit.runner.Description
 
 /**
  *Created by Zaid Zakir
+ * The reason why we need this class is our ShoppingViewModelTest failed , if you look at our ShoppingViewModel class
+ * we use a coroutine inside our fun validateShoppingItem(name: String, amount: String, price: String){} function,
+ * and this calls our insertItemsIntoDb() function and this uses a suspend function, and that uses the MainDispatcher
+ * which relies on the Mainlooper which is only availble in a real app scenario
+ * Since we run this test in our JVM we dont have access to a real apps enviroment like in androidTest and access to MainDispatcher.
+ * So to solve this we need to create our own JUnit Rule.
  */
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
